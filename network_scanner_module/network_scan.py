882ec9309f043539fd7e2bc292ac83a5b5fe6295
Ilya Koy <Ilya.koy@outlook.com>
2019-05-27 19:24:33 -0400

    changed  ping_host to be singular

--- network_scanner_module/network_scan.py
+++ network_scanner_module/network_scan.py
@@ -7,12 +7,18 @@ def network_scan(address="192.168.1.0", subnet=str(30)):
     #Get all usable hosts
     formated_address = address + "/" + subnet
     hosts = list(ipaddress.IPv4Network(formated_address).hosts())
-    
+    results = {}
     #Ping all hosts capture response
-    results = ping_host(hosts)
+    for host in hosts:
+        ping_result = ping_host(host)
+        if ping_result == True:
+            results.setdefault(host.compressed,True)
+        else:
+            results.setdefault(host.compressed,False)
+
     return results
 
-def ping_host(ipaddresses):
+def ping_host(ip_address):
     #Figure out Operating system
     oper = platform.system()
     if (oper == "Windows"):
@@ -21,18 +27,14 @@ def ping_host(ipaddresses):
         ping = "ping -c 1"
     else:
         ping = "ping -c 1"
-    
-    hosts_response = []
-    for address in ipaddresses:
-        formated_ping = ping + ' ' + str(address)
-        print(formated_ping)
-        response = os.popen(formated_ping)
-        for text in response:
-            if text.count('ttl='):
-                hosts_response.append("{} is alive".format(address))
-                
-                
-    return hosts_response
+
+    formated_ping = ping + ' ' + str(ip_address)
+    print(formated_ping)
+    response = os.popen(formated_ping)
+    for text in response:
+        if text.count('ttl='):
+            return True
+    return False
     
 results = (network_scan('192.168.10.0','28'))
 print(results)
