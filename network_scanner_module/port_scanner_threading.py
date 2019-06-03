4547505fdece600b75d12c295704ef2b4cf9e909
Ilya Koy <Ilya.koy@outlook.com>
2019-06-02 20:50:20 -0400

    changed to scan ports without port ranges

--- network_scanner_module/port_scanner_threading.py
+++ network_scanner_module/port_scanner_threading.py
@@ -12,9 +12,9 @@ def TCP_connect(ip, port_number):
         scanner.settimeout(.1)
         scanner.connect((str('google.com'), port_number))
         scanner.close()
-        return [port_number,True]
+        return True
     except:
-        return [port_number,False]
+        return False
 
 
 def scan_ports(host_ip, port_start=None, port_end=None,):
@@ -27,20 +27,9 @@ def scan_ports(host_ip, port_start=None, port_end=None,):
     # Prepare list to capture port ranges for multiple ports
     if port_start != port_end and port_end != None:
         port_ranges = []
-        end_number = port_start
-        for number in range(port_start, port_end+1):
-            if number % 100 == 0:
-                port_ranges.append((end_number, number))
-                end_number = number + 1
-
-        # Go through port ranges
-        for port_range in port_ranges:
-            port_results = port_scan_threader(host_ip, port_range)
-            
-        
-        for result in port_results:
-            print(result)
-
+        for ports in range(port_start,port_end+1):
+            port_ranges.append(ports)
+        results = port_scan_threader(host_ip,port_ranges)
 
     return results
 
@@ -50,21 +39,30 @@ def _multiprocess_port_scan_helper(param):
     return TCP_connect(host_ip,port_numb)
 
 def port_scan_threader(host_ip, port_range):
-    ports = []
-    for numb in range(port_range[0],port_range[1]+1):
+    host_ports = []
+    for numb in port_range:
         host_ip_port = [host_ip,numb]
-        ports.append(host_ip_port)
+        host_ports.append(host_ip_port)
     
     p = Pool()
-    result_list = p.map(_multiprocess_port_scan_helper,ports)
+    result_list = p.map(_multiprocess_port_scan_helper,host_ports)
     p.close()
 
-    return result_list
+    
+    port_dict = {}
+    for idx,result in enumerate(result_list):
+        port_dict[idx+1] = result_list[idx]
+
+
+    return port_dict
     
 
 
 if __name__ == "__main__":
-    assert TCP_connect('google.com',80) == {80:True}
-    assert TCP_connect('google.com',53) == {53:False}
+    assert TCP_connect('google.com',80) == True
+    assert TCP_connect('google.com',53) == False
     
-    scan_ports('google.com',1,100)
+    results = scan_ports('google.com',1,500)
+    assert results[443] == True
+    assert results[80] == True
+    assert results[500] == False
