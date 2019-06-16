d4fb6b5d2efd101aee24046c325be7620a6b4434
Mellivice <ilya.koy@outlook.com>
2019-06-16 15:00:32 -0400

    updating test

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 import sys
 import os
-from network_scanner_module.port_scanner import pscan
+from network_scanner_module.port_scanner_threading import scan_ports,tcp_connect
 
 def test_google_open():
-    test_ip1 = pscan('google.com',80)
-    test_ip2 = pscan('google.com',443)
-    test_ip3 = pscan('google.com',53)
-    assert test_ip1 == "Opened!"
-    assert test_ip2 == "Opened!"
-    assert test_ip3 == "Closed"
+    test_ip1 = tcp_connect('google.com',80)
+    test_ip2 = tcp_connect('google.com',443)
+    test_ip3 = tcp_connect('google.com',53)
+    assert test_ip1 == True
+    assert test_ip2 == True
+    assert test_ip3 == False
+
+def test_port_scan_multiple():
+    results = scan_ports('google.com',1,500)
+    assert results[443] == True
+    assert results[80] == True
+    assert results[500] == False
