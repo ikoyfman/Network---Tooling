6a94c67d812207fbdc4735d0748bb1212693e981
Mellivice <ilya.koy@outlook.com>
2019-06-16 15:41:05 -0400

    working network scanning with hostname

--- network_scanner_module/network_scan.py
+++ network_scanner_module/network_scan.py
@@ -2,11 +2,27 @@ from platform import system
 from ipaddress import IPv4Network
 from subprocess import Popen, PIPE
 
-def network_scan(address="192.168.1.0", subnet=str(24)):
+def network_scan(address="192.168.1.0", subnet=str(24),hostname=False):
     """
     This function is designed to use your OS ping command to ping hosts.
     Will return a dictionary of True and False for each ip address in the subnet
     """
+
+    #Hostname ping
+    if hostname:
+        if system() == 'Windows':
+            ping_arg = '-n'
+        else:
+            ping_arg = '-c'
+        cmd = ['ping', ping_arg, '1', address]
+        
+        proc = Popen(cmd, stdout=PIPE)
+        proc.wait()
+        if proc.returncode == 0:
+            return {address:True}
+        else:
+            return {address:False}
+        
 
     # Get all usable hosts
     formated_address = address + "/" + subnet
