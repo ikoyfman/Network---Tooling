6d8459204e235fa5591bcec61b3cadeb9d3fd0b8
Mellivice <ilya.koy@outlook.com>
2019-06-16 14:49:30 -0400

    removed UDP scanner for now

--- network_scanner_module/port_scanner.py
+++ network_scanner_module/port_scanner.py
@@ -14,16 +14,4 @@ def pscan(ip_name, port, type="SOCK_STREAM"):
         except:
             return "Closed"
 
-    """NOT CURRENTLY WORKING
     # UDP PORT SCAN
-    if type != "SOCK_STREAM":
-
-        try:
-            scanner = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            scanner.bind('127.0.0.1',53)
-            scanner.settimeout(2)
-            
-            return "Opened"
-        except:
-            return "Closed"
-    """          
